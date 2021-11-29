1f144373848c5b40bf6383ea8e1f43f6dca46880
AnaGreco <anaflaviaguedesgreco@hotmail.com>
2021-11-28 22:46:10 -0300

    Varias alteração de recursos

--- controllers/js/login-controller.js
+++ controllers/js/login-controller.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-
 });
 
 function getValidate() {
@@ -163,24 +162,5 @@ function getCadValidate() {
             ano = data.getFullYear();
         return dia + "-" + mes + "-" + ano;
     }
-
-
-    function loading(inicio) {
-
-        if (inicio == true) {
-          swal({
-            title: "Carregando...",
-            text: "Por favor aguarde",
-            icon: "assets/images/plane_loading.gif",
-            button: false,
-            closeOnClickOutside: false,
-            closeOnEsc: false
-          });
-        } else {
-          setTimeout(function () { swal.close(); }, 2000);
-        }
-      
-      }
-    
 
 }
