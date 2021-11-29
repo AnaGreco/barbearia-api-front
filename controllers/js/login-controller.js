528df406baf208b1c003cc73f1af2fe6719bb5e9
AnaGreco <anaflaviaguedesgreco@hotmail.com>
2021-11-28 22:19:52 -0300

    alteração do loading

--- controllers/js/login-controller.js
+++ controllers/js/login-controller.js
@@ -164,4 +164,23 @@ function getCadValidate() {
         return dia + "-" + mes + "-" + ano;
     }
 
+
+    function loading(inicio) {
+
+        if (inicio == true) {
+          swal({
+            title: "Carregando...",
+            text: "Por favor aguarde",
+            icon: "assets/images/plane_loading.gif",
+            button: false,
+            closeOnClickOutside: false,
+            closeOnEsc: false
+          });
+        } else {
+          setTimeout(function () { swal.close(); }, 2000);
+        }
+      
+      }
+    
+
 }
